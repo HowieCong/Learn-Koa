212f7e2c3a0958ec3a8372191fb4202daa9fbf57
hezc7 <hezc7@xiaopeng.com>
2025-08-06 15:40:47 +0800

    初始化项目&01_快速入门

---
+++ src/01_快速入门.js
@@ -0,0 +1,13 @@
+// 一.导入Koa
+const Koa = require ('Koa');
+// 二.实例化app对象
+const app = new Koa();
+// 三、编写中间件
+app.use((ctx) => {
+    // ctx: context http 请求的上下文
+    ctx.body = 'hello koa2'
+})
+// 四、启动服务,监听4000端口
+app.listen(4000, () => {
+    console.log('server is running on http://localhost:4000')
+})
