eaa57ceb691b3e9a5658f5782d5c3c0495736037
hezc7 <howiecong@163.com>
2025-08-11 14:58:10 +0800

    feat: study router/user.router

--- src/07_koa-router.js
+++ src/07_koa-router.js
@@ -2,24 +2,10 @@
 const Koa = require ('koa');
 // 二.实例化app对象
 const app = new Koa();
-// 三、使用koa-router
-// 3.1 导入koa-router包
-const Router = require('koa-router')
-// 3.2 实例化路由对象
-const router = new Router()
-// 3.3 编写路由规则
-router.get('/', (ctx) => {
-    ctx.body = '这是主页'
-})
-router.get('/users', (ctx) => {
-    ctx.body = '这是用户页'
-})
-router.post('/users', (ctx) => {
-    ctx.body = '创建用户'
-})
+// 三、导入router路由
+const userRoute = require('./router/user.route')
 // 四、注册中间件
-app.use( router.routes())
-app.use( router.allowedMethods()) // 处理404和405
+app.use( userRoute.routes()).use( userRoute.allowedMethods()) // 处理404和405
 // 四、启动服务,监听4007端口
 app.listen(4007, () => {
     console.log('server is running on http://localhost:4007')
