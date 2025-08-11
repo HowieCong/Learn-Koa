c0062f7cb9a05b9157a431d90ed9c183b9670fce
hezc7 <howiecong@163.com>
2025-08-11 15:55:33 +0800

    feat: study 通过url如何传参query&params

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -4,14 +4,19 @@ const Router = require('koa-router')
 // 3.2 实例化路由对象
 const router = new Router({prefix: '/users'})
 const db = [
-    {id: 1, name: 'xiaohua', age: 18},
-    {id: 2, name: 'xiaoming', age: 20},
-    {id: 3, name: 'xiaogang', age: 22}
+    {id: 1, name: 'xiaohua', age: 20},
+    {id: 2, name: 'xiaoming', age: 18},
+    {id: 3, name: 'xiaogang', age: 2}
 ]
 // 3.3 编写路由规则
 // GET /users --- 获取所有的用户信息，返回一个数组
 router.get('/', (ctx) => {
-    ctx.body = db
+    // 通过ctx.query 是 ctx.request.query的代理，解析键值对参数
+    const { start = 0, end = 0 } = ctx.query
+    const res = db.filter((item) => item.age >= start && item.age < end)
+    // 解析键值对
+    ctx.body = res
+    res.length === 0 ? ctx.throw(404) : (ctx.body = res)
 })
 // GET /users/:id --- 根据id获取单个用户的信息，返回一个对象
 router.get('/:id', (ctx) => {
