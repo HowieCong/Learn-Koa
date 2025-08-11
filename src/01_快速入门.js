1ac4bd3d3da2a3dffb1dac230cefaaa283c99059
hezc7 <howiecong@163.com>
2025-08-11 19:57:15 +0800

    feat: 中英文注释 01_快速入门

--- src/01_快速入门.js
+++ src/01_快速入门.js
@@ -1,13 +1,21 @@
-// 一.导入Koa
-const Koa = require ('koa');
-// 二.实例化app对象
+// 1. Import Koa
+// 1. 导入Koa
+const Koa = require('koa');
+
+// 2. Instantiate the app object
+// 2. 实例化app对象
 const app = new Koa();
-// 三、编写中间件
+
+// 3. Write middleware
+// 3. 编写中间件
 app.use((ctx) => {
-    // ctx: context http 请求的上下文
+    // ctx: context, the context of the HTTP request
+    // ctx: 上下文，HTTP请求的上下文
     ctx.body = 'hello koa2'
 })
-// 四、启动服务,监听4000端口
+
+// 4. Start the server and listen on port 4001
+// 4. 启动服务，监听4001端口
 app.listen(4001, () => {
     console.log('server is running on http://localhost:4001')
 })
