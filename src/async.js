2aadc243a96853becef0fefc5a2dc7d6a1e718d2
hezc7 <howiecong@163.com>
2025-08-12 15:38:58 +0800

    feat: 中英文注释async&await

--- src/async.js
+++ src/async.js
@@ -1,14 +1,24 @@
-// async await 是关键字
+// async/await are JavaScript keywords
+// async/await 是 JavaScript 的关键字
 
-// async 用来修饰函数，将函数的返回值封装成promise对象
-
-async function foo(){
-    return 123
+// The async keyword makes the function return a Promise
+// async 用来修饰函数，将函数的返回值封装成 Promise 对象
+async function foo() {
+    // Returns 123, but wrapped in a Promise because of async
+    // 返回 123，但由于 async，会被包装成 Promise
+    return 123;
 }
 
-const res = foo() 
-console.log(res)
+// Call the async function; it returns a Promise, not the value 123 directly
+// 调用 async 函数，返回的是 Promise，而不是直接返回 123
+const res = foo();
 
-res.then(data=>{
-    console.log(data);
-})
+console.log(res); // Promise { 123 }
+// 输出 Promise { 123 }
+
+// Use .then to get the resolved value from the Promise
+// 使用 .then 获取 Promise 解析后的值
+res.then(data => {
+    console.log(data); // 123
+    // 输出 123
+});
--- src/await.js
+++ src/await.js
@@ -1,14 +1,24 @@
+// 'await' can only be used inside an 'async' function
 // await 不能单独使用，必须要跟async使用
 
+// 'await' expects a Promise and returns its resolved value
 // await 后面跟一个promise对象，表达式返回一个promise结果
-async function foo(){
-    // const res = await 123
-    const p = new Promise((resolve,reject)=>{
+async function foo() {
+    // Create a new Promise that resolves to 123
+    // 创建一个Promise对象，最终会resolve为123
+    const p = new Promise((resolve, reject) => {
+        // Call resolve to fulfill the promise with value 123
         // 调用resolve，p的结果是123，状态是fulfilled
         resolve(123)
     })
+    // Wait for the promise to resolve and get its value
+    // 等待Promise对象p完成，并获取其结果
     const res = await p
+    // Output the resolved value to the console
+    // 打印Promise的结果到控制台
     console.log(res)
 }
 
+// Call the async function
+// 调用异步函数
 foo()
